647ec05215dac6e89b88588cac269917f331e63a
Дмитрий <dimayadikin1997@gmail.com>
2017-08-17 11:36:45 +0300

    add settings

---
+++ settings.noprm.php
@@ -0,0 +1,14 @@
+<?php
+
+class SETTINGS{
+    
+    const KEY = "";
+    
+    const HOST_DB = "";
+    const USER_DB = "";
+    const PASS_DB = "";
+    const DB = "";
+    
+    const URL_BX24 = "";
+    
+}
